532a5e58f3be7592aa540acf796c932b3eadc6a8
Ali <aliwebdevf@gmail.com>
2024-12-10 15:59:23 +0330

    init

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
-moment.locale("en", { useGregorianParser: true });
+jalaliDatepicker.startWatch({
+  persianDigits: false,
+});
 
-const weekDays = ["ش", "ی", "د", "س", "چ", "پ", "ج"];
-const reverse = weekDays.reverse();
+flatpickr("#flat", { locale: "fa" });
+var picker = new Pikaday({ field: document.getElementById("pick") });
 
-const picker = new Litepicker({
-  element: document.getElementById("litepicker"),
+const elements = document.getElementById(".b");
 
-  setup: (picker) => {
-    picker.on("render:month", (month, date) => {
-      const year = month.querySelector(".month-item-year");
-      const days = month.querySelector(".month-item-weekdays-row");
-
-      days.innerHTML = "";
-
-      reverse.forEach((day) => {
-        let dayDiv = document.createElement("div");
-        dayDiv.textContent = day;
-        dayDiv.classList.add("week-day");
-        days.appendChild(dayDiv);
-      });
-
-      year.style.fontFamily = "Yekan";
-
-      year.textContent = moment(
-        String(Number(year.textContent) + 1),
-        "YYYY/MM/DD"
-      ).jYear();
-    });
-  },
-
-  lang: "fa-IR", // Set the language to Persian (fa-IR)
-  firstDay: 6, // Persian calendar starts on Saturday
-
-  // Disable the conversion (we will handle it ourselves)
-  format: {
-    parse(date) {
-      return moment(date, "en", "YYYY/MM/DD").toDate();
-    },
-
-    output(date) {
-      return moment(date).format("YYYY/MM/DD");
-    },
-  },
+$(document).ready(function () {
+  $("#b").pDatepicker();
 });
